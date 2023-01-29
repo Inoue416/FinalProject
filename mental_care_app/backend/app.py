03c7afae83147fe5708a9fee257875968887eb29
yuyainoue <yuyatuta@icloud.com>
2023-01-29 17:00:18 +0900

    add db setting

--- mental_care_app/backend/app.py
+++ mental_care_app/backend/app.py
@@ -1,4 +1,10 @@
 from flask import Flask, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(
     __name__, 
@@ -6,10 +12,17 @@ app = Flask(
     template_folder='../frontend/dist'
 )
 
-@app.route("/", defaults={"path":""})
-@app.route("/<path:path>")
-def index(path):
-    return render_template("index.html")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DB_URI']
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+from models.users import Users
+from models.things import Things
+from models.points import Points
+
+with app.app_context():
+    db.create_all()
 
 if __name__ in '__main__':
     app.run(debug=True)
