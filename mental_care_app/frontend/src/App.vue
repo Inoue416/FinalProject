1370ce4cc7a72161b46e02bbf8645e13e318f6ae
yuyainoue <yuyatuta@icloud.com>
2023-02-06 17:57:16 +0900

    ナビゲーションバーを日本語に変更後、ルーターリンクのクラスの編集

--- mental_care_app/frontend/src/App.vue
+++ mental_care_app/frontend/src/App.vue
@@ -1,23 +1,26 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <span class="navbar-brand"><router-link to="/">Home</router-link></span>
+      <router-link to="/" class="navbar-brand">はきだしケア</router-link>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <span class="nav-link active"><router-link to="/login">LogIn</router-link></span>
+            <router-link to="/" class="nav-link active">ホーム</router-link>
           </li>
           <li class="nav-item">
-            <span class="nav-link active"><router-link to="/signup">SignUp</router-link></span>
+            <router-link to="/login" class="nav-link active">ログイン</router-link>
           </li>
           <li class="nav-item">
-            <span class="nav-link active"><router-link to="/form">Form</router-link></span>
+            <router-link to="/signup" class="nav-link active">サインアップ</router-link>
           </li>
           <li class="nav-item">
-            <span class="nav-link active"><router-link to="/list">List</router-link></span>
+            <router-link to="/form" class="nav-link active">はきだし</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/list" class="nav-link active">はきだし一覧</router-link>
           </li>
         </ul>
       </div>
