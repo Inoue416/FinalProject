e7b73a840fed8f70a38c87cc805337efb5b08120
yuyainoue <yuyatuta@icloud.com>
2023-02-07 13:13:41 +0900

    パスワード、パスワード確認、メールアドレスのバリデーションを作成

--- mental_care_app/frontend/src/validators/SignupValidator.js
+++ mental_care_app/frontend/src/validators/SignupValidator.js
@@ -1,4 +1,4 @@
-function userNameValidator(value) {
+export function userNameValidator(value) {
     const regexp = /[<>\\\/`~|*!=$&^%()/?.,;:’”＜＞'"\[{\]}\s]+/gi;
     if (!(3 <= value.length && value.length <= 10)){
         return "3 ~ 10文字の範囲で入力してください"
@@ -6,5 +6,24 @@ function userNameValidator(value) {
     else if (value.match(regexp)){
         return "使用できない文字が含まれています";
     }
-    return "1";
+    return null;
+}
+
+export function emailValidator(email) {
+    if (email === null || email === undefined || email.length <= 0) return null; // null: 必須項目であることを表示する
+    const re = /^(([^<>()\\[\]\\.,;:\s@"]+(\.[^<>()\\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email); // true or false. true: 正しい形式, false: 間違っている
+}
+
+export function passwordValidator(password) {
+    if (password === null || password === undefined || password.length <= 0) return null; // null: 必須項目であることを表示する
+    const re = /^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{8,100}$/;
+    return re.test(password);
+}
+
+export function passwordConfirmValidator(password, password_confirm){
+    if (password == password_confirm) {
+        return true;
+    }
+    return false;
 }
