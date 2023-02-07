5496f826e46e91cdd1dc152c9cb47268cb1593a0
yuyainoue <yuyatuta@icloud.com>
2023-02-07 12:41:20 +0900

    ユーザ名入力時のバリデーションの修正

--- mental_care_app/frontend/src/validators/SignupValidator.js
+++ mental_care_app/frontend/src/validators/SignupValidator.js
@@ -3,10 +3,8 @@ function userNameValidator(value) {
     if (!(3 <= value.length && value.length <= 10)){
         return "3 ~ 10文字の範囲で入力してください"
     }
-    else if (value.match(/^[<>/!?.,;:'"[{]}]/)){
+    else if (value.match(regexp)){
         return "使用できない文字が含まれています";
     }
     return "1";
 }
-
-
