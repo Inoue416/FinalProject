b098cd8ad00ebb315fd11529bbc005d75a6861f0
yuyainoue <yuyatuta@icloud.com>
2023-02-08 22:45:07 +0900

    ポイントのテーブルのモデルの修正(トータル、ポジティブ、ネガティブに分けた)

--- mental_care_app/backend/models/points.py
+++ mental_care_app/backend/models/points.py
@@ -5,6 +5,8 @@ class Points(db.Model):
     __tablename__ = "points"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     thing_id = db.Column(db.Integer, nullable=False)
-    point = db.Column(db.Float, nullable=False)
+    total = db.Column(db.Float, nullable=False)
+    positive = db.Column(db.Float, nullable=False)
+    negative = db.Column(db.Float, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now(), nullable=False)
     updated_at = db.Column(db.DateTime, default=datetime.now(), onupdate=datetime.now(), nullable=False)
