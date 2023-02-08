5a278935f761ef7c79a5870771a8ccbc82befc98
yuyainoue <yuyatuta@icloud.com>
2023-02-08 22:44:03 +0900

    感情分類のクラスを作成

--- mental_care_app/backend/EmotionClassification.py
+++ mental_care_app/backend/EmotionClassification.py
@@ -1,5 +1,21 @@
-import asari
+from asari.api import Sonar
 
 class EmotionClassification():
-    def __init__(self, text):
-        pass
+    def __init__(self):
+        self.sonar = Sonar()
+        self.positive = 0
+        self.negative = 0
+    
+    def classification(self, text):
+        if text == "":
+            return None
+        result = self.sonar.ping(text=text)["classes"]
+        try:
+            for value in result:
+                if value["class_name"] == 'positive':
+                    self.positive = value['confidence']
+                    continue
+                self.negative = value['confidence']
+            return True
+        except:
+            return None
