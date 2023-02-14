2784aba7c9dcd2a9d7f4c44793be295eb12767c4
yuyainoue <yuyatuta@icloud.com>
2023-02-14 14:10:18 +0900

    chartのサンプル描画

--- mental_care_app/frontend/src/components/HomeComponent.vue
+++ mental_care_app/frontend/src/components/HomeComponent.vue
@@ -1,11 +1,25 @@
 <script setup>
     import { useStore } from 'vuex';
+    import ChartComponent from './home_components/ChartComponent.vue';
+    //import { ref, mounted } from 'vue';
+   
     const message = "Welcome Home";
     const store = useStore();
-
+    
 </script>
 
 <template>
     <h1>{{ message }}</h1>
     <h2>{{ store.getters["getUserName"] }}</h2>
+    <div class="chart-area border border-dark mt-3">
+        <ChartComponent />
+    </div>
 </template>
+
+<style scoped>
+    .chart-area {
+        width: 100%;
+        height: 40%;
+
+    };
+</style>
---
+++ mental_care_app/frontend/src/components/home_components/ChartComponent.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+  const chartData = {
+    labels: [ 'January', 'February', 'March' ],
+    datasets: [ { data: [40, 20, 12] } ]
+  };
+  const chartOptions = {
+    responsive: true
+  };
+</script>
+
+<template>
+  <Bar
+    id="my-chart-id"
+    :options="chartOptions"
+    :data="chartData"
+  />
+</template>
